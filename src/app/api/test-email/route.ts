ab576c1f8d9164f0d471b57f09ca4ff9a3e7750c
Mika Cornelis <cornelis.mika@gmail.com>
2025-08-19 20:42:53 +0200

    build

--- src/app/api/test-email/route.ts
+++ src/app/api/test-email/route.ts
@@ -1,12 +1,12 @@
-import { Resend } from "resend";
-const resend = new Resend(process.env.RESEND_API_KEY);
+// import { Resend } from "resend";
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function GET() {
-  await resend.emails.send({
-    from: "contact@mika-cornelis.be",
-    to: "cornelis.m@outlook.com",
-    subject: "Test depuis Next.js",
-    text: "TEST EMAIL",
-  });
-  return Response.json({ ok: true });
-}
+// export async function GET() {
+//   await resend.emails.send({
+//     from: "contact@mika-cornelis.be",
+//     to: "cornelis.m@outlook.com",
+//     subject: "Test depuis Next.js",
+//     text: "TEST EMAIL",
+//   });
+//   return Response.json({ ok: true });
+// }
